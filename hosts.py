a3cf49ce2979530724e58b956ec921be84b844c8
Steven Willis <steven@philo.com>
2024-05-16 00:18:00 -0400

    format hosts entries more compactly

--- hosts.py
+++ hosts.py
@@ -4,6 +4,7 @@ import threading
 import time
 import queue
 
+from collections import defaultdict
 import docker
 
 client = docker.from_env()
@@ -20,19 +21,19 @@ def process_container_starts(container_starts, host_updates):
         container_start = container_starts.get()
         container = client.containers.get(container_start["id"])
         for (network_name, network_settings) in container.attrs['NetworkSettings']['Networks'].items():
-            for dns_name in network_settings["DNSNames"]:
-                host_updates.put(
-                    {
-                     "name": f"{dns_name}.{network_name}",
-                     "address": network_settings['IPAddress'],
-                     "definitive": True,
-                    }
-                )
+            host_updates.put(
+                {
+                 "names": [f"{dns_name}.{network_name}" for dns_name in network_settings["DNSNames"]],
+                 "address": network_settings['IPAddress'],
+                 "definitive": True,
+                }
+            )
 
         container_starts.task_done()
 
 def process_host_updates(host_updates):
-    hosts = {}
+    names = set()
+    addresses = defaultdict(dict)
     updated = False
     while True:
         try:
@@ -40,28 +41,28 @@ def process_host_updates(host_updates):
         except queue.Empty:
             if updated:
                 print("# /etc/hosts START")
-                for (name, address) in sorted(hosts.items(), key=lambda x: (x[1], x[0])):
-                    print(f"{address} {name}")
+                for (address, names) in addresses.items():
+                    print(f"{address} {' '.join(names)}")
                 print("# /etc/hosts DONE")
                 updated = False
             continue
-        if host_update["definitive"] or host_update["name"] not in hosts:
-            hosts[host_update["name"]] = host_update["address"]
-            updated = True
+
+        for name in host_update["names"]:
+            if host_update["definitive"] or name not in names:
+                addresses[host_update["address"]][name] = True
+                updated = True
 
 def backfill_all_hosts(host_updates):
     time.sleep(20)
     for container in client.containers.list():
         for (network_name, network_settings) in container.attrs['NetworkSettings']['Networks'].items():
-            print(network_settings["DNSNames"])
-            for dns_name in network_settings["DNSNames"]:
-                host_updates.put(
-                    {
-                     "name": f"{dns_name}.{network_name}",
-                     "address": network_settings['IPAddress'],
-                     "definitive": False,
-                    }
-                )
+            host_updates.put(
+                {
+                 "names": [f"{dns_name}.{network_name}" for dns_name in network_settings["DNSNames"]],
+                 "address": network_settings['IPAddress'],
+                 "definitive": False,
+                }
+            )
 
 gather_events_thread = threading.Thread(target=gather_events, args=(container_starts,), daemon=False)
 process_container_starts_thread = threading.Thread(target=process_container_starts, args=(container_starts, host_updates), daemon=True)
